fb909fa96c4d2efcf55ba7d2f7ef4b07cf941f2d
Greg <g.kamut@gmail.com>
2017-02-06 22:10:37 +0100

    Initial commit.

---
+++ src/main/java/com/jgosh/MiniCrmApplication.java
@@ -0,0 +1,12 @@
+package com.jgosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCrmApplication.class, args);
+	}
+}
